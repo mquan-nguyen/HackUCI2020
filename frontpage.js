a9c5a3dee50b42b6ba9a026010cce5194eb98e06
mquan-nguyen <mnguy1231@gmail.com>
2020-02-01 09:14:26 -0800

    frontpage - now looking better

--- frontpage.js
+++ frontpage.js
@@ -15,10 +15,7 @@ function updateSliderValues() {
   }
 }
 
-$(document).ready(function () {
-  $(".dropdown-menu li a").click(function(){
-    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-  });
-});
-
+function updateDropdown(html_object) {
+  topDropdown = document.getElementById("select-button");
+  topDropdown.innerHTML = html_object.innerHTML;
+}
