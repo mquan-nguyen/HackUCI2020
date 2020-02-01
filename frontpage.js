caeb1963e4b40f518be12826b338ad9ec66aa2ea
mquan-nguyen <mnguy1231@gmail.com>
2020-02-01 09:05:12 -0800

    Rename files to frontpage

--- frontpage.js
+++ frontpage.js
@@ -1,4 +1,3 @@
-updateValues()
 
 var sliderArray = {"range1": "output1",
                     "range2": "output2"}
@@ -7,7 +6,7 @@ console.log(sliderArray);
 // Update the current slider value (each time you drag the slider handle)
 
 
-function updateValues() {
+function updateSliderValues() {
   for (var sliderID in sliderArray) {
 
     var slider = document.getElementById(sliderID);
@@ -15,3 +14,11 @@ function updateValues() {
     output.innerHTML = slider.value;
   }
 }
+
+$(document).ready(function () {
+  $(".dropdown-menu li a").click(function(){
+    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+  });
+});
+
